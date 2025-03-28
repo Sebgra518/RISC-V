e515769c8ff5e5bb8a4f584ad97766d78a1bfe98
Sebastian Graciano <sebgra518@gmail.com>
2025-03-27 18:51:39 -0700

    Update: Decode Module. Working on immediate

--- project_1/project_1.srcs/sources_1/new/InstructionDecode.v
+++ project_1/project_1.srcs/sources_1/new/InstructionDecode.v
@@ -1,10 +1,15 @@
 `timescale 1ns / 1ps
 
-module InstructionDecode(instruction,rs1,rs2,rd);
+module InstructionDecode(instruction,funct7,rs2,rs1,funct3,rd,opcode);
     input wire [31:0] instruction;
-    output reg [5:0] rs1;
-    output reg [5:0] rs2;
-    output reg [31:0] rd;
+    
+    output reg [7:0] funct7;
+    output reg [4:0] rs2;
+    output reg [4:0] rs1;
+    output reg [2:0] funct3;
+    output reg [4:0] rd;
+    output reg [6:0] opcode;
+    
     
     reg is_u_instruction;
     reg is_r_instruction;
@@ -21,16 +26,36 @@ module InstructionDecode(instruction,rs1,rs2,rd);
         is_i_instruction = ((instruction[6:2] == 5'b00_000) || (instruction[6:2] == 5'b00_001) || (instruction[6:2] == 5'b00_100) || (instruction[6:2] == 5'b00_110) || (instruction[6:2] == 5'b11_001));
         is_b_instruction = (instruction[6:2] == 5'b11_000);
         
-        if(is_u_instruction)begin
+        opcode  = instruction[6:0];
+       
+       
+       if(!is_u_instruction || !is_j_instruction) begin
+            funct3 = instruction[14:12];
+            rs1 = instruction[19:15];
             
-        end
-        if(is_r_instruction)begin
-            rs2     = instruction[24:20];
-            rs1     = instruction[19:15];
-            funct3  = instruction[14:12];
-            rd      = instruction[11:7];
-            opcode  = instruction[6:0];
-        end
+            if(is_i_instruction)begin
+                rd = instruction[11:7];
+            end else begin
+                rs2 = instruction[24:20]; 
+                
+                if(is_r_instruction)begin
+                    funct7 = instruction[31:25];
+                    rd = instruction[11:7];
+                end
+                
+            end
+            
+       end else begin
+            rd = instruction[11:7];
+       end
+       
+       
+            
+            
+            
+            
+            
+            
     end
     
     
