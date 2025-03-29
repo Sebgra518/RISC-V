3dd4b793a9f7d83da7b3300eb919f54fa45c1cc1
Sebastian Graciano <sebgra518@gmail.com>
2025-03-28 21:54:05 -0700

    Finished Imm

--- project_1/project_1.srcs/sources_1/new/InstructionDecode.v
+++ project_1/project_1.srcs/sources_1/new/InstructionDecode.v
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-module InstructionDecode(instruction,funct7,rs2,rs1,funct3,rd,opcode);
+module InstructionDecode(instruction,funct7,rs2,rs1,funct3,rd,opcode,imm);
     input wire [31:0] instruction;
     
     output reg [7:0] funct7;
@@ -9,55 +9,65 @@ module InstructionDecode(instruction,funct7,rs2,rs1,funct3,rd,opcode);
     output reg [2:0] funct3;
     output reg [4:0] rd;
     output reg [6:0] opcode;
+    output reg [31:0] imm;
     
-    
-    reg is_u_instruction;
-    reg is_r_instruction;
-    reg is_s_instruction;
-    reg is_j_instruction;
-    reg is_i_instruction;
-    reg is_b_instruction;
 
     always @ * begin
-        is_u_instruction = ((instruction[6:2] == 5'b00_101) || (instruction[6:2] == 5'b01_101));
-        is_r_instruction = ((instruction[6:2] == 5'b01_011) || (instruction[6:2] == 5'b01_100) || (instruction[6:2] == 5'b01_110) || (instruction[6:2] == 5'b10_100));
-        is_s_instruction = ((instruction[6:2] == 5'b01_000) || (instruction[6:2] == 5'b01_001));
-        is_j_instruction = (instruction[6:2] == 5'b11_011);
-        is_i_instruction = ((instruction[6:2] == 5'b00_000) || (instruction[6:2] == 5'b00_001) || (instruction[6:2] == 5'b00_100) || (instruction[6:2] == 5'b00_110) || (instruction[6:2] == 5'b11_001));
-        is_b_instruction = (instruction[6:2] == 5'b11_000);
         
         opcode  = instruction[6:0];
        
        
-       if(!is_u_instruction || !is_j_instruction) begin
+       //R Type
+       if((instruction[6:2] == 5'b01_011) || (instruction[6:2] == 5'b01_100) || (instruction[6:2] == 5'b01_110) || (instruction[6:2] == 5'b10_100)) begin
+            rd = instruction[11:7];
+            funct3 = instruction[14:12];
+            rs1 = instruction[19:15];
+            rs2 = instruction[24:20];
+            funct7 = instruction[31:25];
+       end
+       
+       //I Type
+       if((instruction[6:2] == 5'b00_000) || (instruction[6:2] == 5'b00_001) || (instruction[6:2] == 5'b00_100) || (instruction[6:2] == 5'b00_110) || (instruction[6:2] == 5'b11_001))begin
+            rd = instruction[11:7];
             funct3 = instruction[14:12];
             rs1 = instruction[19:15];
             
-            if(is_i_instruction)begin
-                rd = instruction[11:7];
-            end else begin
-                rs2 = instruction[24:20]; 
-                
-                if(is_r_instruction)begin
-                    funct7 = instruction[31:25];
-                    rd = instruction[11:7];
-                end
-                
-            end
-            
-       end else begin
-            rd = instruction[11:7];
+            imm = {{20{instruction[31]}}, instruction[31:20]};
+
        end
        
+       //S Type
+       if((instruction[6:2] == 5'b01_000) || (instruction[6:2] == 5'b01_001)) begin
+            funct3 = instruction[14:12];
+            rs1 = instruction[19:15];
+            rs2 = instruction[24:20];
+            
+            imm = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};
+       end
        
+       //U Type
+       if((instruction[6:2] == 5'b00_101) || (instruction[6:2] == 5'b01_101)) begin
+            rd = instruction[11:7];
+
+            imm = {instruction[31:12], 12'b0};
+       end
+       
+       //J Type
+       if(instruction[6:2] == 5'b11_011) begin 
+            rd = instruction[11:7];
             
-            
-            
-            
-            
-            
+            imm = {{11{instruction[31]}}, instruction[31], instruction[19:12], instruction[20], instruction[30:21], 1'b0};
+
+       end
+       
+       //B Type
+       if(instruction[6:2] == 5'b11_000) begin
+            funct3 = instruction[14:12];
+            rs1 = instruction[19:15];
+            rs2 = instruction[24:20];
+            funct7 = instruction[31:25];
+       end
+      
     end
-    
-    
     
 endmodule
